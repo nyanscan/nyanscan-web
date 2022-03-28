1c1fd6163c2ac015cd472451d83d4b8ef6535a12
Alice.B <boyeralice0@gmail.com>
2022-03-28 20:40:44 +0200

    :sparks: start login/register

---
+++ auth/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Connection | NyanScan</title>
+    <link rel="stylesheet" type="text/css" href="../css/nyanscan.css">
+</head>
+<body>
+    <div id="login" style="background-color: darkcyan">
+        <form method="post" action="login.php">
+            <input type="text" name="user" placeholder="Adresse mail" required="required">
+            <input type="password" name="password" placeholder="mot de passe" required="required">
+            <input type="submit" name="login" value="Connexion">
+        </form>
+    </div>
+    <div id="register">
+        <form method="post" action="register.php">
+            <input type="text" name="username" placeholder="Nom d'utilisateur" required="required">
+            <input type="text" name="email" placeholder="Adresse mail" required="required">
+            <input type="password" name="password" placeholder="mot de passe" required="required">
+            <input type="password" name="password-v" placeholder="verification" required="required">
+            <label>
+                News Letter:
+                <input type="checkbox" name="newsletter" required="required">
+            </label>
+            <label>
+                CGU:
+                <input type="checkbox" name="cgu" required="required">
+            </label>
+            <input type="submit" name="register" value="S'enregister">
+        </form>
+    </div>
+</body>
+</html>
