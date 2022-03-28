706d60e1a69e5577159d66a4041c9926c0059a2d
Alice.B <boyeralice0@gmail.com>
2022-03-28 14:01:01 +0200

    :tada: add dev

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<meta charset="UTF-8">
+	<style>
+		body {
+			background-color: magenta;
+			min-height: 100vh;
+		}
+		marquee {
+			color: red;
+			font-size: 40px
+		}
+
+		#ps {
+			font-size: 12px;
+			position: fixed;
+			bottom: 0;
+		}
+
+	</style>
+</head>
+<body>
+	<center>
+		<h2> CouCou Monsieur :) </h2>
+		<p> On attend que vous validiez notre projet pour remplire cette page ! </p>
+		<marquee> C'est sympa les marquee quand même ! (Merci M. SKRZYPCZYK ;) ) </marquee>
+		<br>
+		<p>
+			"Je suis peut-être pas aussi fort que mon grand frére mais regardez les belles pages que je sais faire ;)" <br> <br>
+				Cordialement <br>
+				Grégoire Decrouez
+		</p>
+		<p>
+			Alice .B - Mélissa. G - Grégoire .D 
+		</p>
+	</center>
+	<p id="ps">
+		Oui on vas pas laisser les marquee et les center <3 ;)
+	</p>
+
+</body>
+</html>
