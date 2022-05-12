2be310677a4b0edbb747cd0fcd68b60b281f0afb
Alice.B <boyeralice0@gmail.com>
2022-05-13 01:00:38 +0200

    :bookmark: fix db_adapter and complete forumController POST AND GET

--- private/utils/forum.php
+++ private/utils/forum.php
@@ -2,41 +2,29 @@
 require 'const.php';
 require($_SERVER['DOCUMENT_ROOT'] . '/private/utils/functions.php');
 
-function create_category($title, $description, $view_level, $create_level, $db = null)
+function create_category($title, $description, $view_level, $create_level)
 {
-    $db = $db ?: connectDB();
-    $req = $db->prepare("INSERT INTO PAE_FORUM_CATEGORY (name, description, permission) VALUE (:name, :description, :permission)");
-    $req->execute([
+    getDB()->insert(TABLE_FORUM_CATEGORY, [
         "name" => $title,
         "description" => $description,
         "permission" => ($view_level & 0b1111) | (($create_level & 0b1111) << 4)
     ]);
 }
 
-function delete_category($id, $db = null)
+function delete_category($id) : bool
 {
-    $db = $db ?: connectDB();
-    $req = $db->prepare("DELETE FROM PAE_FORUM_CATEGORY WHERE id=?");
-    $req->execute([$id]);
+    return getDB()->delete(TABLE_FORUM_CATEGORY, ["id" => $id]);
 }
 
-function get_category($id, $column = ["id, name, description"], $db = null)
+function get_category($id, $column = ["id, name, description"])
 {
-    $db = $db ?: connectDB();
-    if (is_numeric($id)) {
-        $req = $db->prepare("SELECT " . join(', ', $column) . " FROM PAE_FORUM_CATEGORY WHERE id=? LIMIT 1");
-    } else {
-        return null;
-    }
-    $req->execute([$id]);
-    return $req->fetch(PDO::FETCH_ASSOC);
-
+    if (!is_numeric($id)) return null;
+    return getDB()->select('FORUM_CATEGORY', $column, ["id" => $id], 1);
 }
 
-function get_all_visible_category($view_level, $column = ["id, name, description"], $db = null)
+function get_all_visible_category($view_level, $column = ["id, name, description"])
 {
-    $db = $db ?: connectDB();
-    $req = $db->prepare("SELECT " . join(', ', $column) . " FROM PAE_FORUM_CATEGORY WHERE (permission & :mask) <= :level");
+    $req =getDB()->get_pdo()->prepare("SELECT " . join(', ', $column) . " FROM PAE_FORUM_CATEGORY WHERE (permission & :mask) <= :level");
     $req->execute([
         "mask" => FORUM_PERMISSION_VIEW_MASK,
         "level" => $view_level & FORUM_PERMISSION_VIEW_MASK
