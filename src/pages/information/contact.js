6809c4e2c7acf735f5d73e0b565ea496baab85f3
Alice.B <boyeralice0@gmail.com>
2022-07-04 02:35:50 +0200

    random change

---
+++ src/pages/information/contact.js
@@ -0,0 +1,19 @@
+export default class extends Pages {
+    title = "Page CGU | NyanScan"
+
+    get raw() {
+        return `
+    <section class="ns-background-image-cgu">
+        <div class="ns-scan-CGU">
+            <div class="text-center"><h1 class="ns-fs-1">Contact</h1></div>
+            
+        </div>
+    </section>
+      `;
+    }
+
+    constructor(app) {
+        super(app);
+    }
+
+}
